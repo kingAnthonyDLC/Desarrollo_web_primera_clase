c524538badb3a1e4e439194df9081fdc5d545cdb
kingAnthony <delacruzanthony058@gmail.com>
2025-11-20 23:15:20 -0500

    cambios_imagenes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: 
+  [IonHeader, IonToolbar, 
+    IonTitle, IonContent],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+
+
 })
 export class HomePage {
   constructor() {}
+
+  message: string | null = null;
+
+  showMessage() {
+    this.message = '¡Felicidades! Has creado tu primera app con ionic';
+  }
+
 }
