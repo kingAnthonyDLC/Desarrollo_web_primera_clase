aedda27b285512603c502c34e413391b961a1caf
kingAnthony <delacruzanthony058@gmail.com>
2025-11-24 18:41:47 -0500

    claseDía24

--- src/app/paginas/principal/principal.page.ts
+++ src/app/paginas/principal/principal.page.ts
@@ -1,7 +1,7 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardSubtitle, IonCardTitle, IonButton} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-principal',
