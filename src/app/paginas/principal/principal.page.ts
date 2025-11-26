e022d9cfa6ab1c6063e7aa7957f466361ad82ca5
kingAnthony <delacruzanthony058@gmail.com>
2025-11-25 20:03:08 -0500

    claseDía25_Vermas

--- src/app/paginas/principal/principal.page.ts
+++ src/app/paginas/principal/principal.page.ts
@@ -2,6 +2,7 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardSubtitle, IonCardTitle, IonButton} from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-principal',
@@ -13,9 +14,19 @@ import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardSubtitle, 
 })
 export class PrincipalPage implements OnInit {
 
-  constructor() { }
-
+  constructor(
+    private router: Router
+  
+  ) { }
+    
+  
   ngOnInit() {
   }
+
+
+  irvermas() {
+  this.router.navigate(['/ver-mas']);    
+
+}
 
 }
